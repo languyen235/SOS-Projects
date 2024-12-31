afcc44abffa3474690a93a4baa9ef83cd4f1da65
Linh Nguyen <linh.a.nguyen@intel.com>
2024-12-31 01:53:31 -0800

    Updated _init_ function for setting SOS_SERVERS_DIR and Web URL of test, production and replica servers Changed all print() to logger Searching for ERROR and higher levels in the log and sending email at end of script.

--- sos-monitoring/util_scripts/modules/utils.py
+++ sos-monitoring/util_scripts/modules/utils.py
@@ -45,7 +45,7 @@ def file_older_than(file_path: str | os.PathLike, day: int = 1):
     return False
 
 
-def increase_disk_size(disk_name: str, adding: int) -> str:
+def increase_disk_size(disk_name: str, adding_size: int) -> bool:
     """Increase disk size using START command
     1. Expected output for success:
     Your request is being processed
@@ -53,26 +53,39 @@ def increase_disk_size(disk_name: str, adding: int) -> str:
     """
     _THIS_SITE: str = subprocess.getoutput('echo $EC_ZONE')
 
-    # <size> /(2**30) return  GB from Bytes; // division and returns the integer part of the result (rounding down)
+    # <size> / (2**30) converts bytes to Gb and keep only integer, discard the decimal part
     size = (shutil.disk_usage(disk_name)[0]) // (2**30)
 
-    # nearest hundredth or tenth digit
-    by_number = 100 if size >= 1000 else 10
-    new_size = (size // by_number * by_number) + adding  # the // returns only integer
+    # by_number = 100 if size >= 1000 else 10
+    # new_size = (size // by_number * by_number) + adding_size  # the // returns only integer
+    #
+    #  rounding down to the nearest 1000 or 100
+    # decimal_factor = -3 if size >= 1000 else -2
+    if size >= 1000:
+        decimal_factor = -3
+    elif size >= 100:
+        decimal_factor = -2
+    else:
+        logger.debug('Original disk size is less than 100GB')
+        return False
 
-    logger.info(f"-I- New disk size:  Size({size}Gb) + adding({adding}Gb) = {new_size}Gb")
+    #  rounding down size to the nearest 1000 or 10 (for example: 501 -> 500, 1024 -> 1000)
+    rounded_down_value = round(size, decimal_factor)
+    new_size = rounded_down_value + adding_size
+    logger.debug(f"New disk size:  Size({size}Gb) + adding({adding_size}Gb) = {new_size}Gb")
+
     stod_cmd = f"/usr/intel/bin/stod resize --cell {_THIS_SITE} --path {disk_name} " \
                f"--size {new_size}GB --immediate --exceed-forecast"
-    logger.info("%s",stod_cmd)
 
     try:
         p = subprocess.run(stod_cmd, capture_output=True, check=True, text=True, shell=True)
-        if result := re.search(r'successfully.*$', p.stdout, re.I):
-            return f"-I- {result.group()}"
-        else:
-            return f"-F- Failure to resize disk: {p.stderr}"
+        if result := re.search(r'(successfully).*$', p.stdout, re.I):
+            logger.info("%s", result.group())
+            return True
     except subprocess.CalledProcessError as er:
-        return f"-F- Exception occurred: {er.stderr}"
+        logger.debug("START command: %s",stod_cmd)
+        logger.error("START command failed with error: %s", er.stderr)
+        return False
 
 
 def has_disk_size_been_increased(disk_info: str, day: int=2) -> bool | None:
@@ -85,22 +98,24 @@ def has_disk_size_been_increased(disk_info: str, day: int=2) -> bool | None:
     3) Timeout error from START
     Error: request timed out, please try with --timeout switch
     """
-    d_name = Path(disk_info).name
-    # cmd: str = '/usr/intel/bin/stodstatus requests --field Type,SubmitTime --format csv --history ' + \
-    #     f"{day}d --number 1 \"description=~'{d_name}' && type=~'resize'\""
-
-    cmd: str = "/usr/intel/bin/stodstatus requests --field Type,SubmitTime --format csv --history " \
-                f"{day}d --number 1 \"description=~'{d_name}' && type=~'resize'\""
-    logger.info("%s",cmd)
+    disk_name = Path(disk_info).name
+    stod_request_cmd: str = "/usr/intel/bin/stodstatus requests --field Type,SubmitTime --format csv --history " \
+                f"{day}d --number 1 \"description=~'{disk_name}' && type=~'resize'\""
 
     try:
-        p = subprocess.run(cmd, capture_output=True, check=True, shell=True, text=True)
-        logger.debug("%s",p.stdout)
+        p = subprocess.run(stod_request_cmd, capture_output=True, check=True, shell=True, text=True)
         # match_true = re.search(r"stod\s+resize", p.stdout)
         # match_none = re.search(r"Type,SubmitTime", p.stdout)
-        return re.search(r"stod\s+resize", p.stdout.strip())
+        # search for "stod resize" in the output
+        if search_result := re.search(r"stod\s+resize,\d{2}/\d{2}/\d{4}\s+\d{2}:\d{2}:\d{2}", p.stdout):
+            logger.info("%s", search_result.group())
+            return True
+        elif search_result is None:
+            logger.info("Disk %s was not increased in the last %s days", disk_name, day)
+            return None
     except subprocess.CalledProcessError as er:
-        logger.error("Exception occurred: %s", er.stderr)
+        logger.debug("START command: %s",stod_request_cmd)
+        logger.error("START command failed with error: %s",er.stderr)
         return False
 
 
@@ -121,11 +136,12 @@ def exclude_services(exclu_file: str | os.PathLike, services: List) -> List[str]
     for line in filter(None, lines.split('\n')):  # filter empty and split line
         if not line.startswith('#'):
             try:
+                logger.debug("Excluding service: %s", line)
                 new_list.remove(line)  # remove service from the list
             except ValueError:
                 logger.warning("%s is not in service list, skipping...", line)
 
-    logger.info('Excluding service(s): ',
+    logger.debug('Excluding service(s): ',
           [x for x in filter(None, lines.split('\n')) if not x.startswith('#')])
 
     return new_list
@@ -178,3 +194,46 @@ def sosgmr_status(url)-> Tuple[str, int | str]:
         return 'Failure', "sosmgr timed out"
     else:
         return 'Success', response.getcode()
+
+
+def parse_error_messages(log_file: str | os.PathLike)-> List[str]:
+    """Parse messages from log file"""
+    messages = []
+    with open(log_file, 'r', encoding='utf-8') as file:
+        for line in file:
+            # if re.search(r'ERROR|WARNING|FATAL|CRITICAL|EXCEPTION', line):
+            if re.search(r'todo_when_low_disk_space|has_disk_size_been_increased|increase_disk_size', line):
+                messages.append(line)
+    return messages
+
+
+def verify_file_link(file_link, expected_directory):
+    # Check if the file link exists
+    if not os.path.exists(file_link):
+        logger.error(f"The file link '{file_link}' does not exist.")
+        return False
+
+    # Get the absolute path of the file link
+    file_link_abs_path = os.path.abspath(file_link)
+
+    # Get the absolute path of the expected directory
+    expected_directory_abs_path = os.path.abspath(expected_directory)
+
+    # Check if the file link is a symbolic link
+    if os.path.islink(file_link):
+        # Get the target of the symbolic link
+        target_path = os.readlink(file_link)
+        target_abs_path = os.path.abspath(target_path)
+
+        # Verify if the target path is within the expected directory
+        if target_abs_path.startswith(expected_directory_abs_path):
+            logger.info(f"The file link '{file_link}' "
+                        f"is correctly linking to the expected directory '{expected_directory}'.")
+            return True
+        else:
+            logger.error(f"The file link '{file_link}' "
+                         f"is not linking to the expected directory '{expected_directory}'.")
+            return False
+    else:
+        logger.error(f"The file link '{file_link}' is not a symbolic link.")
+        return False
--- sos-monitoring/util_scripts/sos_check_disk_usage.py
+++ sos-monitoring/util_scripts/sos_check_disk_usage.py
@@ -9,57 +9,71 @@ import logging
 from functools import wraps
 from pathlib import Path
 from typing import List, Iterator, Tuple, Final, TextIO
-from modules.utils import send_email, sosgmr_status, lock_script, report_disk_size
+from modules.utils import (send_email, sosgmr_status,
+                           lock_script, report_disk_size, parse_error_messages, verify_file_link)
 
 # Global variables
-LOW_DISK_LIMIT: Final[int] = 250  # threshold for low disk size
+LOW_SPACE_LIMIT: Final[int] = 250  # threshold for low disk size
+ADDED_SPACE_LIMIT: Final[int] = 10  # threshold for disk size increase
 
 class ClioService:
     """Setup Cliosoft application service"""
-    sites = ['sc','sc1', 'sc4', 'sc8', 'pdx', 'iil', 'png', 'iind', 'altera_sc', 'altera_png', 'vr']
+    sites = ['sc','sc1', 'sc4', 'sc8', 'pdx',
+             'iil', 'png', 'iind', 'altera_sc', 'altera_png', 'vr']
     ddm_contacts = ['linh.a.nguyen@intel.com']
     sender = "linh.a.nguyen@intel.com"
 
     # Directory for data and logs
-    data_path = Path('/opt/cliosoft/monitoring/DATA')
-    log_path = Path('/opt/cliosoft/monitoring/LOGS')
+    data_path = Path('/opt/cliosoft/monitoring/data')
+    log_path = Path('/opt/cliosoft/monitoring/logs')
+    log_file = Path(log_path, 'sos_check_disks.log')
     for path in [data_path, log_path]:
         if path.exists() is False:
             path.mkdir(mode=0o775, parents=True, exist_ok=True)
 
     def __init__(self, site):
         self.site = site
-        self.get_site_name = subprocess.getoutput('sosmgr site get -u | head -1', encoding='utf-8')
-        logger.debug("Site name: %s", self.get_site_name)
+        self.sever_config_link = Path('/opt/cliosoft/latest/SERVERS')
+        self.primary_server_config_folder = Path('/nfs/site/disks/sos_adm/share/SERVERS7')
+        self.replica_server_config_folder = Path('/nfs/site/disks/sos_adm/share/SERVERS8-replica')
+        self.web_url = ''
+        os.environ['SOS_SERVERS_DIR'] = ''
 
-        if re.search(r'replica', self.get_site_name):
-            os.environ['SOS_SERVERS_DIR'] = '/opt/cliosoft/latest/SERVERS'
-            logger.debug("Using SOS_SERVERS_DIR: %s", os.environ['SOS_SERVERS_DIR'])
-            self.web_url = f"http://plxs1209.pdx.intel.com:3080"
-        elif self.site in ClioService.sites or re.match(r'^zsc\d+$', self.site):
+        if self.site == 'sc':   # <-- this is necessary because SC has both production and test servers
             os.environ['SOS_SERVERS_DIR'] = '/nfs/site/disks/sos_adm/share/SERVERS7'
-            logger.debug("Using SOS_SERVERS_DIR: %s", os.environ['SOS_SERVERS_DIR'])
             self.web_url = f"http://sosmgr-{self.site}.sync.intel.com:3070"
+        elif self.site in ClioService.sites or re.match(r'^zsc\d+$', self.site):
+            if verify_file_link(self.sever_config_link, self.primary_server_config_folder):
+                os.environ['SOS_SERVERS_DIR'] = '/nfs/site/disks/sos_adm/share/SERVERS7'
+                self.web_url = f"http://sosmgr-{self.site}.sync.intel.com:3070"
+            elif verify_file_link(self.sever_config_link, self.replica_server_config_folder):
+                os.environ['SOS_SERVERS_DIR'] = '/nfs/site/disks/sos_adm/share/SERVERS8-replica'
+                self.web_url = f"http://sosmgr-{self.site}-replica.sync.intel.com:3080"
         else:
+            os.environ['SOS_SERVERS_DIR'] = '/opt/cliosoft/latest/SERVERS'
             self.web_url = 'http://scysync36.sc.intel.com:8080'  # this test server URL
 
+        self.site_name = subprocess.getoutput('sosmgr site get -u | head -1', encoding='utf-8')
         self.data_file = Path(ClioService.data_path, f"{self.site.upper()}_cliosoft_disks.txt")
 
 
-def run_subprocess( cmd, timeout ):
+def run_subprocess(cmd, timeout: int) -> List[str]:
     """Run subprocess with timeout"""
     process = subprocess.Popen(shlex.split(cmd),
                                stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True,
                                universal_newlines=True)
     try:
         std_out, std_err = process.communicate(timeout=timeout)
+        print("Stdout:", std_out)
+        print("Stderr:", std_err)
+        print("Return code:", process.returncode)
         if process.returncode != 0:
-            def remove_empty_lines(string):
-                return re.sub(r'(?<=\n)\s+', '', string , re.MULTILINE)
-            logger.critical("Command [%s] failed: \n%s", cmd, remove_empty_lines(std_err))
+            # def remove_empty_lines(string):
+            #     return re.sub(r'(?<=\n)\s+', '', string , re.MULTILINE)
+            logger.critical("Command [%s] failed: \n%s", cmd, std_out)
             sys.exit(1)
     except subprocess.TimeoutExpired:
-        logger.critical("Subprocess timed out")
+        logger.critical("%s function timed out", run_subprocess.__name__)
         process.kill()
         process.communicate()
         raise TimeoutError(f"Command: [{cmd}] timed out")
@@ -74,6 +88,7 @@ def get_sos_services() -> List[str]:
     try:
         services = run_subprocess(sos_cmd, timeout=10)
         if not services:
+            logger.error("No SOS services found")
             raise ValueError('No SOS services found')
         else:
             return services
@@ -113,10 +128,10 @@ def decorator_create_data_file(func: callable) -> callable:
 
 
 def get_parent_dir(disk_path: Path) -> Path:
-    """Get parent directory of disk path relatives to pg_data folder"""
+    """Get specific disk path that relatives to pg_data folder"""
     path = Path(disk_path, 'pg_data')
-    root_level = 5  #  5 elements: ('/', 'nfs', 'site', 'disks', 'hipipde_soscache_013')
-    level = len(path.parents) - root_level
+    root_path_level = 5  #  5 elements: ('/', 'nfs', 'site', 'disks', 'hipipde_soscache_013')
+    level = len(path.parents) - root_path_level
     return path.parents[level]
 
 
@@ -135,14 +150,13 @@ def create_data_file(data_file: Path) -> None:
         for row in get_sos_disks():
             if re.match(r'site', row[0]):
                 continue # skip header row
-            if len(row) > 5:   # has replica path
+            if len(row) > 5:   # contains replica path
                 replica_dir = get_parent_dir(Path(row[-1]))
                 write_disk_path(replica_dir, found_disks, file)
             else:
                 repo_dir = get_parent_dir(Path(row[-1]))
                 locality = row[-2].lower()
                 cache_dir = get_parent_dir(Path(row[-3]))
-                # cache_cmd = f"stodstatus areas --for csv --fie StandardPath \"path=~'{Path(row[-3])}'\" | tail -1"
                 if locality == 'local':
                     write_disk_path(repo_dir, found_disks, file)
                 write_disk_path(cache_dir, found_disks, file)
@@ -152,14 +166,15 @@ def create_data_file(data_file: Path) -> None:
 
 
 def disk_space_info(file: str | Path)-> Tuple[Tuple[str], Tuple[str]]:
-    """ Check disk space and return list of low space disks """
+    """ Check disk space and return disk info and low disk space disks"""
     disks = Path(file).read_text(encoding='utf-8').strip().splitlines()
     disk_info_all = ()
     low_space_disks = ()
+    # find available disk space
     for disk in sorted(disks, key=os.path.basename):
         size, used, avail = report_disk_size(disk)
         disk_info_all += ([disk, size, used, avail],)
-        if avail <= LOW_DISK_LIMIT:
+        if avail <= LOW_SPACE_LIMIT:
             low_space_disks += ([disk, size, used, avail],)
     return disk_info_all, low_space_disks
 
@@ -174,30 +189,29 @@ def write_to_csv_file(csv_file, data: Tuple[str]) -> None:
             csv_writer.writerow(row)
 
 
-def todo_when_low_disk_space(problem_disks: Tuple, added_size: int) -> List[str]:
+def todo_when_low_disk_space(problem_disks: Tuple, added_size: int) -> None:
     """
     Iterate over a tuple of disks for disk name, size and space.
     Generate messages for low disk space and recent disk size increases.
     With option to increase disk size.
     """
     from modules.utils import has_disk_size_been_increased, increase_disk_size
-    messages = []
     for line in problem_disks:
         disk, size, _, avail = line
-        messages.append(f"-W- {disk} Size={size}GB; Avail={avail}GB *** Low disk space")
+        # messages.append(f"-W- {disk} Size={size}GB; Avail={avail}GB *** Low disk space")
+        # logger.warning("%s Size=%sGB; Avail=%sGB *** Low disk space", disk, size, avail)
 
         if result := has_disk_size_been_increased(disk, day=2):
-            print(result)
-            messages.append(result)
-            messages.append(f"-W- {disk} size was increased recently...Need investigation")
+            logger.warning("%s Size=%sGB; Avail=%sGB *** Low disk space", disk, size, avail)
+            logger.warning("%s's size was increased recently...Need investigation", disk.split('/')[-1])
         elif result is None:
             if added_size > 0:
-                messages.append(f"Before: {disk}: {report_disk_size(disk)[0]}GB")
-                messages.append(increase_disk_size(disk, added_size))
-                messages.append(f"After: {disk}: {report_disk_size(disk)[0]}GB")
+                increase_disk_size(disk, added_size)
+            else:
+                logger.warning("%s Size=%sGB; Avail=%sGB *** Low disk space", disk, size, avail)
         else:
-            messages.append('-E- Disk check failed')
-    return messages
+            logger.warning("%s Size=%sGB; Avail=%sGB *** Low disk space", disk, size, avail)
+            logger.error('Disk check failed')
 
 
 def check_web_status(web_url: str, site: str):
@@ -205,13 +219,13 @@ def check_web_status(web_url: str, site: str):
     mgr_status = sosgmr_status(web_url)
     logger.debug(mgr_status)
     if mgr_status[0] == 'Failure':
-        logger.debug("sosmgr web service is inaccessible")
+        logger.error("sosmgr web %s is inaccessible", web_url)
         error_msg = f"{site.upper()} {mgr_status[1]}"
         subject = f"Cliosoft Alert: {site.upper()} sosmgr web service is inaccessible"
-        send_alert(subject, [error_msg] , ClioService.ddm_contacts, ClioService.sender)
+        send_alert_email(subject, [error_msg], ClioService.ddm_contacts, ClioService.sender)
 
 
-def send_alert(subject, messages, recipients, sender):
+def send_alert_email(subject, messages, recipients, sender):
     send_email(subject, messages, recipients, sender)
 
 
@@ -241,13 +255,15 @@ def main() -> None:
 
     ### uncomment for testing script on test site
     # sos = ClioService('ddm')  # test site
-    # sos.web_url = "http://scysync36.sc.intel.com:8080"
     #### End
 
     ## Uncomment this block for production site
     this_site = subprocess.getoutput('echo $EC_ZONE', encoding='utf-8')
     sos = ClioService(this_site)
     ## End
+    logger.debug("Setting SOS_SERVERS_DIR to: %s", os.environ['SOS_SERVERS_DIR'])
+    logger.debug("Site name: %s", sos.site_name.rstrip(':'))
+    logger.debug("Web URL: %s", sos.web_url)
 
     # check if we need to refresh data file
     if is_new_data_file or (sos.data_file.exists() and sos.data_file.stat().st_size == 0):
@@ -258,32 +274,43 @@ def main() -> None:
     # check sosmgr FE web service
     check_web_status(sos.web_url, sos.site)
 
-    # check disk space and increase disk size
+    # check disk space and optionally increase disk size
     list_disks, low_space_disks = disk_space_info(sos.data_file)
     if low_space_disks:
-        adding_size: int = 10 if is_adding_space else 0
-        status_msg = todo_when_low_disk_space(low_space_disks, adding_size)
-        logger.debug(status_msg)
-        subject = f"Cliosoft Alert: {sos.site.upper()} disk is low on space"
-        send_alert(subject, status_msg, ClioService.ddm_contacts, ClioService.sender)
+        adding_space = ADDED_SPACE_LIMIT if is_adding_space else 0
+        # status_msg = todo_when_low_disk_space(low_space_disks, adding_size)
+        # logger.debug(status_msg)
+        # subject = f"Cliosoft Alert: {sos.site.upper()} disk is low on space"
+        # send_alert(subject, status_msg, ClioService.ddm_contacts, ClioService.sender)
+        todo_when_low_disk_space(low_space_disks, adding_space)
     else:
         logger.debug("All disks have enough space")
+
+
+    # messages = parse_error_messages(ClioService.log_file)
+    if messages := parse_error_messages(ClioService.log_file):
+        subject = f"Cliosoft Alert: {sos.site.upper()} disk space check results"
+        send_alert_email(subject, messages, ClioService.ddm_contacts, ClioService.sender)
+    else:
+        logger.debug("%s disk space check passed", sos.site.upper())
 
     # save disk usages to csv file
     csv_file = Path(ClioService.data_path, f"{sos.site.upper()}_disk_usages.csv")
     write_to_csv_file(csv_file, list_disks)
 
+    logger.info('Script finished')
+
 
 if __name__ == '__main__':
-    lock_file = "/tmp/sos_checkdisk.lock"
+    lock_file = "/tmp/sos_check_disks.lock"
     lock_fd = lock_script(lock_file)
-    log_file = Path(ClioService.log_path, 'sos_check_disk.log')
-    logging.basicConfig(level=logging.INFO,
-                        # filename=Path(ClioService.log_path, 'sos_check_disk.log'),
+    log_file = Path(ClioService.log_path, 'sos_check_disks.log')
+    log_level = os.environ.get('LOG_LEVEL', 'DEBUG').upper() # Default 'INFO' if LOG_LEVEL is not set
+    logging.basicConfig(level=log_level,
                         format='[%(asctime)s] [%(name)s] [%(funcName)s] [%(levelname)s] %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S',
                         handlers=[
-                            logging.FileHandler(log_file),  # Create a file handler
+                            logging.FileHandler(log_file, mode='w'),  # Create a file handler
                             logging.StreamHandler()  # Create a console handler
                         ])
     logger = logging.getLogger(__name__)
@@ -292,5 +319,4 @@ if __name__ == '__main__':
     # Release the lock (optional)
     os.close(lock_fd)
     os.unlink(lock_file)
-    logger.info('Script finished')
 
