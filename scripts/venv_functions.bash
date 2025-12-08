06c1f1de57974a44fa37a8e96c177736db160d71
Linh Nguyen <linh.a.nguyen@intel.com>
2025-12-08 13:14:14 -0800

    Code enhacements
        modified:   scripts/venv_functions.bash

--- scripts/venv_functions.bash
+++ scripts/venv_functions.bash
@@ -3,30 +3,30 @@
 # Description: Utility functions for creatingPython virtual environments base on SLES version
 
 # Constants
-declare -r BASE_DIR="/opt/cliosoft/monitoring"
-declare -r SERVICE_DIR="/opt/cliosoft/monitoring/replica_service"
-[[ -d "$SERVICE_DIR" ]] && mkdir -p "$SERVICE_DIR"
 
-# declare -r SRC_DIR="/opt/cliosoft/monitoring/tests"
-# declare -r SCRIPT_NAME="tests.py"
+declare -r BASE_DIR="/opt/cliosoft/monitoring"
+declare VENV_LOG_FILE="$BASE_DIR/logs/venv_functions.log"
+declare -r SCRIPT_DIR="$BASE_DIR/scripts"
+[[ ! -d $BASE_DIR/logs ]] && mkdir -p "$BASE_DIR/logs"
+
+declare -r DEFAULT_INTERPRETER="/usr/intel/bin/python3"
+delare -r  ALTERNATE_INTERPRETER='/usr/intel/pkgs/python3/3.12.3/bin/python3'
+
+# Set proxy
+export HTTP_PROXY=http://proxy-dmz.intel.com:912
+export HTTPS_PROXY=http://proxy-dmz.intel.com:912
+
 declare VENV_PATH
 declare OS_VERSION
-declare -r DEFAULT_PYTHON_FILE="/usr/intel/bin/python3"
-declare LOG_FILE="$SERVICE_DIR/venv_functions.log"
-# Set proxy
-export HTTPS_PROXY=http://proxy-dmz.intel.com:912
-export HTTPS_PROXY=http://proxy-dmz.intel.com:912
 
 # Function to activate Python venv based on OS
-verify_os_version() {
-  local os_version
-
+get_os_version() {
   # Get OS version from /etc/os-release
-  os_version=$(grep VERSION_ID /etc/os-release | cut -d'=' -f2 | tr -d '"')
+  log_debug "Verifying OS version..."
+  OS=$(grep VERSION_ID /etc/os-release | cut -d'=' -f2 | tr -d '"')
 
-  # Set virtual environment path
-  # For SLES12 and SLES15.4 use venv_312; Otherwise use venv_313 (python 3.13.2)
-  case "$os_version" in
+  # Set Python virtual environment folder location by OS version
+  case "$OS" in
     12*)
       VENV_PATH="$BASE_DIR/venv_312"
       OS_VERSION="SLES12"
@@ -40,115 +40,148 @@ verify_os_version() {
       OS_VERSION="SLES15-SP7"
       ;;
   esac
+  log_debug "OS: $OS_VERSION; VENV path: $VENV_PATH"
+}
+
+create_python_venv() {
+  # Create virtual environment for Python whether OS SLES12 or SLES15
+
+  case "$OS_VERSION" in
+    12*)
+      mypython3="$ALTERNATE_PYTHON_FILE"
+      ;;
+    15.4)
+      mypython3="$ALTERNATE_PYTHON_FILE"
+      ;;
+    15.7)
+      mypython3="$DEFAULT_INTERPRETER"
+      ;;
+    *)
+      log_error "Unsupported OS version: $OS_VERSION"
+      return 1
+  esac
+
+  if ! $mypython3 -m venv "$VENV_PATH"; then
+    log_error "Failed to create virtual environment"
+    return 1
+  fi
+
+  log_debug "Python version: $($mypython3 --version)"
+  return 0
 }
 
 activate_python_venv() {
   # Activate virtual environment
-  if [[ -f "${VENV_PATH}/bin/activate" ]]; then
-    log "INFO" "Activating: ${VENV_PATH}"
-    source "${VENV_PATH}/bin/activate"
-    return 0
-  else
-    log "ERROR" "venv not found: $BASE_DIR" >&2
+  if ! source "${VENV_PATH}/bin/activate"; then
+    log_error "Failed to activate virtual environment"
     return 1
   fi
+
+  log_debug "Python version: $(python3 --version)"
+  return 0
 }
 
-create_python_venv() {
-  # Create virtual environment
-  echo "Creating virtual environment: $VENV_PATH"
-  if [[ ${OS_VERSION} == "SLES12" || ${OS_VERSION} == "SLES15-SP4" ]]; then
-    mypython3='/usr/intel/pkgs/python3/3.12.3/bin/python3'
-  elif [[ ${OS_VERSION} == 'SLES15-SP7' ]]; then
-    mypython3='/usr/intel/bin/python3'
-  else
-    log "ERROR" "Unsupported OS version: $OS_VERSION" >&2
+
+check_requests_lib() {
+  # Check if requests library is installed
+  if ! python3 -c "import requests"; then
+    log_error "Requests library is not installed"
     return 1
   fi
 
-  if ! $mypython3 -m venv "$VENV_PATH"; then
-    echo "ERROR: Failed to create virtual environment" >&2
-    return 1
-  fi
+  log_debug "Requests library is installed"
+  return 0
 }
 
 
 install_python_libs() {
   # Install Python requirements.txt
-  check_reqs=$(python3 -c "import requests" && echo "Passed" || echo "Failed")
-  # log "INFO" "Requests module check result: $check_reqs" >&2
-  if [[ $check_reqs == "Failed" ]]; then
-    log "INFO" "Installing requirements.txt"
-    if ! python3 -m pip install -r "$BASE_DIR/requirements.txt"; then
-      log "ERROR" "Failed to install Python requirements" >&2
-      return 1
-    fi
+  if ! python3 -m pip install -r "$BASE_DIR/requirements.txt"; then
+    log_error "Failed to install Python requirements"
+    return 1
   fi
+
+  log_debug "Python requirements installed successfully"
+  return 0
 }
 
 
 run_with_venv() {
-  # Run script with venv
+  # Run script with python venv
   local args=("$@")
-
-  if [[ $(which python3) == "$DEFAULT_PYTHON_FILE" ]]; then
-    log "ERROR" "Failed to run Python in virtual environment" >&2
-    return 1
-  else
-    log "INFO" "Python virtual environment path: $(which python3)"
-    python3 "${args[@]}"
-  fi
-}
-
-log() {
-  local level="$1"
-  local message="${*:2}"
-  local timestamp
-  timestamp=$(date +"%Y-%m-%d %H:%M:%S")
-  echo "$timestamp [$level] $message" | tee -a "$LOG_FILE"
+  python3 "${args[@]}"
+  log_debug "Script executed successfully"
+  deactivate
+  return 0
 }
 
 
 main() {
   local args=("$@")
-
-  log "DEBUG" "Verifying OS version"
-  verify_os_version
-  log "INFO" "OS version: $OS_VERSION" >&2
-  log "DEBUG" "Virtual environment path: $VENV_PATH" >&2
+  get_os_version
 
   # Create virtual environment
-  if [[ ! -d "$VENV_PATH" ]]; then
-    log "INFO" "Creating virtual environment at $VENV_PATH"
-    create_python_venv
+  if [[ -d "$VENV_PATH" ]]; then
+    log_debug "Activating virtual environment..."
+    if ! activate_python_venv; then
+      log_error "Failed to activate virtual environment"
+      exit 1
+    fi
   else
-    log "DEBUG" "Virtual environment already exists at $VENV_PATH"
+    return_codes=()
+    log_debug "Creating virtual environment at $VENV_PATH"
+    create_python_venv
+    return_codes+=($?)
+    activate_python_venv
+    return_codes+=($?)
+    install_python_libs
+    return_codes+=($?)
+
+    for code in "${return_codes[@]}"; do
+      if [[ $code -ne 0 ]]; then
+          log_error "Failed to create virtual environment"
+          exit 1
+      fi
+    done
   fi
 
-  # Activate virtual environment and install Python required libs if missing
-  log "DEBUG" "Activating virtual environment..."
-  if ! activate_python_venv; then
-    # log "ERROR" "Failed to activate virtual environment"
-    exit 1
-  fi
-
-  log "INFO" "Installing/verifying Python libraries..."
-  if ! install_python_libs; then
-    # log "ERROR" "Failed to install Python libraries"
-    exit 1
-  fi
-
-  # Run script with venv
-  if ! run_with_venv "${args[@]}"; then
-    # log "ERROR" "Command failed with exit code $?"
-    exit 1
-  fi
+  # Run script in virtual environment
+  run_with_venv "${args[@]}"
 }
 
-  log "INFO" "Script executed successfully"
+help() {
+  echo "Usage: $0 <Python script> [python script arguments] [-d|--debug]"
+}
 
-#----
+
+#----------------
+# Main execution block
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
-    main "$@"
-fi
+  # Default values
+  DEBUG=0
+  args=("$@")
+  echo "args: ${args[*]}"
 
+  # Check for debug flag in any position
+  for arg in "$@"; do
+    if [[ "$arg" == "--debug" || "$arg" == "-d" ]]; then
+        DEBUG=1
+        # export LOG_LEVEL=DEBUG
+        # Remove debug flag from arguments so it doesn't get passed to Python script
+        unset "args[-1]"
+        break
+    fi
+  done
+
+  # Source logging functions with the determined debug level
+  source "$SCRIPT_DIR/log_functions.bash" "$DEBUG" "$VENV_LOG_FILE"
+
+  # If no arguments provided, show help
+  if [[ ${#args[@]} -eq 0 ]]; then
+    help
+    exit 1
+  fi
+
+  # Pass remaining arguments to main
+  main "${args[@]}"
+fi
